64c0163c79df6c8db8405e90b48cfef0a8e1d72e
Joel Darling <joel.darling@gmail.com>
2016-04-22 19:43:36 -0400

    refined the GET by ID test

--- tests/server/routes/product-test.js
+++ tests/server/routes/product-test.js
@@ -26,7 +26,7 @@ describe('Product Route', function () {
     var product;
 
     beforeEach(function (done) {
-  		Product.create({name: 'Product', price: '5'})
+  		Product.create({name: 'Product', price: 5})
       .then(function(_product_){
         product = _product_;
         done();
@@ -43,7 +43,10 @@ describe('Product Route', function () {
     it('can retrieve an product by ID', function(){
       return request.get('/api/products/' + product._id)
       .then(function(res){
+
         expect(res.body.length).to.equal(1);
+        expect(res.body.name).to.equal('Product');
+        expect(res.body.price).to.equal(5);
       });
     });
 
