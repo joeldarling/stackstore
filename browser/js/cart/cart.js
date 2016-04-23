7fd887455db4cb12207e84228d419d0d9737e0d1
Joel Darling <joel.darling@gmail.com>
2016-04-23 18:51:22 -0400

    halfway making price work

--- browser/js/cart/cart.js
+++ browser/js/cart/cart.js
@@ -17,23 +17,35 @@ app.config(function ($stateProvider) {
 app.controller('CartController', function($scope, cart, OrderFactory){
 
   $scope.cart = cart;
+  $scope.total = getTotal(cart.products);
 
   $scope.increaseQty = function(product){
-    console.log(cart._id, product);
     return OrderFactory.addOne(cart._id, product)
     .then(function(res){
       $scope.cart = res;
+      $scope.total = getTotal($scope.cart.products);
     });
   };
 
   $scope.decreaseQty = function(product){
-    console.log(cart._id, product);
-
     return OrderFactory.removeOne(cart._id, product)
     .then(function(res){
       $scope.cart = res;
+      $scope.total = getTotal($scope.cart.products);
     });
 
   };
 
 });
+
+function getTotal(products){
+
+  var total;
+
+  products.forEach(function(item){
+    console.log(item)
+    total += (+item.price * +item.quantity);
+  });
+
+  return total;
+}
