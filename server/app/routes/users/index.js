386fe77b61555071a0e49819d3158907e548cd9b
Joel Darling <joel.darling@gmail.com>
2016-04-27 18:38:36 -0400

    user query now returns limited data and order info with populated product info

--- server/app/routes/users/index.js
+++ server/app/routes/users/index.js
@@ -16,23 +16,6 @@ router.get('/', function(req, res, next){
 	.then(null, next);
 });
 
-// router.get('/:id', function(req, res, next){
-//
-// 	var userToReturn = {};
-//
-// 	User.findOne({_id: req.params.id}, {email: 1, address: 1})
-// 	.populate('address')
-// 	.then(function(user){
-// 		userToReturn = user;
-// 		return Order.find({user: req.params.id});
-// 	})
-// 	.populate('products')
-// 	.then(function(orders){
-//
-// 		res.json({user: userToReturn, orders: orders});
-// 	})
-// 	.then(null, next);
-// });
 router.get('/:id', function(req, res, next){
 
 	var userToReturn = {};
