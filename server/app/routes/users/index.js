e3e5f9e15bfb5ff9cdf34def9dfcbb5e128cb05c
Ontima Chung <ontima@gmail.com>
2016-05-02 13:52:49 -0400

    added admin user functions

--- server/app/routes/users/index.js
+++ server/app/routes/users/index.js
@@ -7,6 +7,7 @@ var Promise = require('bluebird');
 
 var User = mongoose.model('User');
 var Order = mongoose.model('Order');
+var Address = mongoose.model('Address');
 
 router.get('/', function(req, res, next){
 	User.find({})
@@ -38,4 +39,66 @@ router.get('/:id', function(req, res, next){
 		res.json({user: result[0], orders: result[1]});
 	});
 
+});
+
+router.post('/', function(req, res, next){
+	var newAddress = new Address({
+		address: req.body.address,
+		city: req.body.city,
+		state: req.body.state,
+		zip: req.body.zip
+	});
+	newAddress.save()
+	.then(function(address){
+		var newUser = new User({
+			email: req.body.email,
+			address: address._id,
+			passwordReset: false,
+			status: req.body.status
+		});
+		newUser.save()
+		.then(function(response){
+			res.send(response);
+		});
+	})
+});
+
+router.put('/status/:id', function(req, res, next){
+	User.findOne({_id: req.params.id})
+	.then(function(user){
+		user.status = req.body.status;
+		return user.save();
+	})
+	.then(function(response){
+		res.send(response);
+	}, next);
+});
+
+router.put('/passwordreset/:id', function(req, res, next){
+	User.findOne({_id: req.params.id})
+	.then(function(user){
+		user.passwordReset = true;
+		return user.save();
+	})
+	.then(function(response){
+		res.send(response);
+	}, next);
+});
+
+router.put('/password/:id', function(req, res, next){
+	User.findOne({_id: req.params.id})
+	.then(function(user){
+		user.password = req.body.password;
+		return user.save();
+	})
+	.then(function(response){
+		res.send(response);
+	}, next);
+});
+
+router.delete('/:id', function(req, res, next){
+	User.findOneAndRemove({_id: req.params.id})
+	.then(function(response){
+		res.send(response);
+	}, next);	
 });
